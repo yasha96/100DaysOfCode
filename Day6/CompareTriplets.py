a58cd837ee131f2fa8d63a1ddbcb34b8c1c0cfcd
Yashaswini Raj <yasha9.6raj@gmail.com>
2023-08-18 16:45:45 +0530

    Adding for Day5

---
+++ Day6/CompareTriplets.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def compareTriplets(a, b):
+    # Write your code here
+    alice_score=0
+    bob_score=0
+    for i in range(3):
+        if a[i] > b[i]:
+            alice_score += 1
+        elif a[i] < b[i]:
+            bob_score += 1
+    return alice_score, bob_score
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
